98d3e18aa1c6de41565ba0cd459e87bb2cadd6bc
mahajanjayesh <officialjayesh@gmail.com>
2016-09-15 17:34:29 +0530

    Update book.js

--- bookrestapi/controllers/book.js
+++ bookrestapi/controllers/book.js
@@ -3,30 +3,30 @@ var router = express.Router();
 var bookService = require('../logic/bookService');
 
 router.get('/book', function (request, response) {
-	console.log("in findAll");
+	console.log("Used findAll");
 	response.send(bookService.findAll());
 });
 
 router.get('/book/:bookId', function (request, response){
-	console.log("in findOne : " + request.params.bookId);
+	console.log("Used findOne : " + request.params.bookId);
 	var bookId = request.params.bookId;	
 	var returnBook = bookService.getUserById(bookId);
 	response.send(returnBook);
 });
 
 router.post('/book', function (request, response){
-	console.log("in save");
+	console.log("Used Save");
 	response.send(bookService.save(request.body));
 });
 
 router.put('/book/:bookId', function (request, response){
-	console.log("in update");
+	console.log("Used Update");
 	response.send('HTTP Method "PUT" is called');
 });
 
 router.delete('/book/:bookId', function (request, response){
 	console.log("in delete");
-	response.send('HTTP Method " DELETE" is called');
+	response.send('HTTP Method "DELETE" is called');
 });
 
 module.exports = router;
